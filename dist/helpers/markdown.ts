c7496ebde26e6d3514b9529f9456d0aac25b4d9a
mochacup <104591560+pawtals@users.noreply.github.com>
2025-10-24 18:07:54 -0400

    Add small markdown helper

---
+++ dist/helpers/markdown.ts
@@ -0,0 +1,23 @@
+import MarkdownIt from "markdown-it/index.js";
+
+class Markdown {
+    private markdown: MarkdownIt;
+    private mainString: string;
+    constructor() {
+        this.mainString = "";
+        this.markdown = new MarkdownIt();
+    }
+
+    public AddLine(line: string) {
+        this.mainString += line + "  \n";
+        return;
+    }
+
+    public Render(): string {
+        return this.markdown.render(this.mainString);
+    }
+
+    get MarkdownString(): string {
+        return this.mainString;
+    }
+}
